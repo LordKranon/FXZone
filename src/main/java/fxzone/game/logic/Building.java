de2f8e6f0a2d07ca6b2bbc30d6fa3e79a0ad67f1
LordKranon <kirill@gratchev.com>
2024-05-23 17:12:36 +0200

    added button to factory ui

--- src/main/java/fxzone/game/logic/Building.java
+++ src/main/java/fxzone/game/logic/Building.java
@@ -10,6 +10,7 @@ import fxzone.game.logic.serializable.BuildingSerializable;
 import fxzone.game.render.GameObjectBuilding;
 import java.awt.Color;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
@@ -51,8 +52,8 @@ public class Building extends TileSpaceObject{
 
     private void initializeConstructionMenuUI(){
         constructionMenu = new VBox();
-        constructionMenu.setPrefWidth(300);
-        constructionMenu.setPrefHeight(500);
+        constructionMenu.setPrefWidth(256);
+        constructionMenu.setPrefHeight(512);
         constructionMenu.setVisible(true);
         constructionMenu.setStyle("-fx-background-color: #282828;");
         constructionMenu.setViewOrder(ViewOrder.GAME_BUILDING_UI_BACKGROUND);
@@ -61,11 +62,21 @@ public class Building extends TileSpaceObject{
         unitIcon.setImage(AssetHandler.getImageUnit(new KeyUnit(UnitType.INFANTRY, 0, Color.CYAN)));
         unitIcon.setFitWidth(128);
         unitIcon.setFitHeight(128);
-        unitIcon.setTranslateX(100);
-        unitIcon.setTranslateY(100);
+        unitIcon.setTranslateX(0);
+        unitIcon.setTranslateY(0);
         unitIcon.setVisible(true);
         unitIcon.setViewOrder(ViewOrder.GAME_BUILDING_UI_BUTTON);
         constructionMenu.getChildren().add(unitIcon);
+
+        Button unitPurchaseButton = new Button();
+        unitPurchaseButton.setPrefWidth(128);
+        unitPurchaseButton.setPrefHeight(128);
+        unitPurchaseButton.setTranslateX(128);
+        unitPurchaseButton.setTranslateY(-128);
+        unitPurchaseButton.setVisible(true);
+        unitPurchaseButton.setViewOrder(ViewOrder.GAME_BUILDING_UI_BUTTON);
+        unitPurchaseButton.setText("Buy");
+        constructionMenu.getChildren().add(unitPurchaseButton);
     }
     public VBox getConstructionMenu(){
         return constructionMenu;
