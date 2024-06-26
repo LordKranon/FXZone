1cdc396d7df3d247bab84c522090ff3d5c2a004a
LordKranon <kirill@gratchev.com>
2024-06-26 20:36:15 +0200

    tile icons in editor

--- src/main/java/fxzone/controller/ingame/InGameEditorUiController.java
+++ src/main/java/fxzone/controller/ingame/InGameEditorUiController.java
@@ -1,6 +1,9 @@
 package fxzone.controller.ingame;
 
 import fxzone.engine.controller.AbstractGameController;
+import fxzone.engine.handler.AssetHandler;
+import fxzone.engine.handler.KeyTile;
+import fxzone.engine.handler.KeyUnit;
 import fxzone.engine.utils.ViewOrder;
 import fxzone.game.logic.Tile;
 import fxzone.game.logic.TileType;
@@ -10,6 +13,7 @@ import fxzone.game.logic.serializable.MapSerializable;
 import fxzone.game.logic.serializable.TileSerializable;
 import fxzone.save.Save;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class InGameEditorUiController extends InGameUiController{
 
@@ -41,9 +45,20 @@ public class InGameEditorUiController extends InGameUiController{
         super.createFXSceneUI();
 
         this.editorTileTypePlaced = TileType.PLAINS;
+
+        ImageView tileIcon = new ImageView();
+        tileIcon.setImage(AssetHandler.getImageTile(new KeyTile(editorTileTypePlaced)));
+        tileIcon.setFitWidth(75);
+        tileIcon.setFitHeight(75);
+        tileIcon.setTranslateX(50);
+        tileIcon.setTranslateY(600);
+        tileIcon.setVisible(true);
+        tileIcon.setViewOrder(ViewOrder.UI_BUTTON);
+        escapeMenu.getChildren().add(tileIcon);
+
         Button tileTypeButton = new Button(""+editorTileTypePlaced);
         tileTypeButton.setPrefWidth(400);
-        tileTypeButton.setTranslateX(100);
+        tileTypeButton.setTranslateX(150);
         tileTypeButton.setTranslateY(600);
         tileTypeButton.setViewOrder(ViewOrder.UI_BUTTON);
         tileTypeButton.setVisible(true);
@@ -54,6 +69,7 @@ public class InGameEditorUiController extends InGameUiController{
                 editorTileTypePlaced = TileType.PLAINS;
             }
             tileTypeButton.setText(""+editorTileTypePlaced);
+            tileIcon.setImage(AssetHandler.getImageTile(new KeyTile(editorTileTypePlaced)));
         });
         escapeMenu.getChildren().add(tileTypeButton);
 
